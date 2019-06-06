e2eac632bce81384ad6ccc3233fa02faa24883e0
Mikhail Shardin <kriptonit1988@gmail.com>
2019-06-06 16:09:55 +0500

    Updates

---
+++ precache-manifest.e8fdc0aa92732ed6f1f0e9fa6b68c9a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31bd7a89112558d0dcbf95da753fda4c",
+    "url": "/re-store/index.html"
+  },
+  {
+    "revision": "e9f1a2f3108c76c2f033",
+    "url": "/re-store/static/css/main.5b7bf5b2.chunk.css"
+  },
+  {
+    "revision": "cfd504ee46d11824db80",
+    "url": "/re-store/static/js/2.ee20d010.chunk.js"
+  },
+  {
+    "revision": "e9f1a2f3108c76c2f033",
+    "url": "/re-store/static/js/main.1ee80ed1.chunk.js"
+  },
+  {
+    "revision": "5d6fe284d288fb554198",
+    "url": "/re-store/static/js/runtime~main.d45e52b3.js"
+  }
+]);
